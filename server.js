0ccce741ee9688bd454ad244ba0a52a187ca38b4
Gigi bajadze <you@example.com>
2025-05-02 18:51:14 +0400

    add port on env file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
 
-
+const port = process.env.PORT || 3000;
 
 
 
 app.listen(3000, () =>{
-    console.log('server is running on port 3000');
+    console.log(`Server is running on port ${port}`);
 })
