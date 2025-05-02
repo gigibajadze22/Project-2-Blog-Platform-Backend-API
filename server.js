e680374f292297570dc98ad60118795b286b82a6
Gigi bajadze <you@example.com>
2025-05-02 19:22:35 +0400

    add app.get

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import app from "./app.js";
 
 const port = process.env.PORT || 3000;
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 app.listen(3000, () =>{
     console.log(`Server is running on port ${port}`);
